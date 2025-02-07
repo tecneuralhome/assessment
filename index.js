e6c2a8a0421ddc073b652e45e573fa787fcb1b93
Aashik <aashikbsc@gmail.com>
2025-02-06 21:08:18 -0800

    updated change

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var express = require("express");
 const cors = require("cors");
 var bodyParser = require("body-parser");
-const Yaml = require('yamljs');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = Yaml.load("./docs/api.yaml");
 const dotenv = require("dotenv");
 const path = require('path');
 dotenv.config();
@@ -13,7 +10,6 @@ var route = require("./routes/route");
 var app = express();
 app.use(cors({origin: '*'}));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', route);
 app.set("port", config.port);
 app.listen(app.get("port"), () =>
